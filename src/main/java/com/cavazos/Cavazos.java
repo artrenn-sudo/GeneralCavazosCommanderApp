06f99305c5a586be013d7678a29e05f9382392f3
Renn Le <rennlttmy@gmail.com>
2026-04-01 15:01:30 -0700

    refactor: fix indentation to 4 spaces

--- src/main/java/com/cavazos/Cavazos.java
+++ src/main/java/com/cavazos/Cavazos.java
@@ -4,21 +4,21 @@ import org.json.simple.*;
 
 public class Cavazos {
 
-  // Print command array
-  public static void print(String[] commandArray) {
-    System.out.printf("Number\tCommand\n");
-    System.out.printf("------\t---------------\n");
-    for (int i = 0; i < commandArray.length; i++) {
-      System.out.printf("%04d\t%s\n", i, commandArray[i]);
+    // Print command array
+    public static void print(String[] commandArray) {
+        System.out.printf("Number\tCommand\n");
+        System.out.printf("------\t---------------\n");
+        for (int i = 0; i < commandArray.length; i++) {
+            System.out.printf("%04d\t%s\n", i, commandArray[i]);
+        }
     }
-  }
 
-  // Convert JSONArray to String array
-  public static String[] getCommandArray(JSONArray commandArray) {
-    String[] arr = new String[commandArray.size()];
-    for (int i = 0; i < commandArray.size(); i++) {
-      arr[i] = commandArray.get(i).toString();
+    // Convert JSONArray to String array
+    public static String[] getCommandArray(JSONArray commandArray) {
+        String[] arr = new String[commandArray.size()];
+        for (int i = 0; i < commandArray.size(); i++) {
+            arr[i] = commandArray.get(i).toString();
+        }
+        return arr;
     }
-    return arr;
-  }
 }
